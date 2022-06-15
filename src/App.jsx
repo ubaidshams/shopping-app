11cb0dac0a9128a9fb8e5b77f6dc1d111a41482f
amruta <amrutasadalgi@gmail.com>
2022-06-15 17:45:03 +0530

    log commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import CustomRoutes from "./routes/CustomRoutes";
+import Login from "./pages/auth/Login"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <CustomRoutes />
       <Footer />
+      <Login/>
     </Router>
   );
 };
