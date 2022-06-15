1d9c25027a12539655f2cba2dce808db6573dd7e
Ubaid Uz Zaman <wahidshams@gmail.com>
2022-06-15 23:11:08 +0530

    auth block completed

--- src/components/navbar/Auth.jsx
+++ src/components/navbar/Auth.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from "./navbar.module.css";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Auth = () => {
   return (
-    <div>
-      <Link to="/login">Log-in</Link>
+    <div className={styles.authBlock}>
+      <Link to="/cart" className={styles.cartIcon}>
+        <AiOutlineShoppingCart />
+        <span>0</span>
+      </Link>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
     </div>
   );
 };
