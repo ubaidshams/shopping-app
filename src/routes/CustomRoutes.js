3cc6c0f63fe6ec312e6937bacbe7039fac127516
Ubaid Uz Zaman <wahidshams@gmail.com>
2022-06-15 20:45:56 +0530

    started navbar

--- src/routes/CustomRoutes.js
+++ src/routes/CustomRoutes.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useRoutes } from "react-router";
 import Home from "../pages/home/Home";
+import Login from "./../pages/auth/Login";
+import Signup from "./../pages/auth/Signup";
 
 const CustomRoutes = () => {
   let myRoutes = useRoutes([
     {
       path: "/",
       element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
     },
   ]);
   return myRoutes;
