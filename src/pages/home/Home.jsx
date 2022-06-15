b3f11d485b73f2894abf6af7e7574ad4cb4d64ba
Ubaid Uz Zaman <wahidshams@gmail.com>
2022-06-15 17:01:57 +0530

    second commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>hello</p>
+      <h4>yo</h4>
+    </div>
+  );
 };
 
 export default Home;
