cf12989d361fa600a1c86f131258a5f7ec034899
SivanSrivishnu <mallikasrivishnu@gmail.com>
2022-06-16 10:34:04 +0530

    signupPage

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 import { Card, TextField, makeStyles } from "@material-ui/core";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
