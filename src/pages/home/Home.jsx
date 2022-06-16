e2862f1d22302f2b61a0088f0e1e3b9d0582c359
Ubaid Uz Zaman <wahidshams@gmail.com>
2022-06-16 10:16:20 +0530

    tried fetching products

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axios } from "axios";
 
 const Home = () => {
+  let [product, setProduct] = useState([]);
+  useEffect(() => {
+    let fetchProducts = async () => {
+      try {
+        let { products } = await fetch("https://dummyjson.com/products").then(
+          res => res.json()
+        );
+        setProduct(products);
+      } catch (error) {
+        console.log(error);
+      }
+      console.log(product);
+    };
+    fetchProducts();
+  }, []);
   return (
     <div>
-      <h1>HomePage</h1>
-      <p>hello</p>
-      <h4>yo</h4>
+      {product.map(prod => {
+        let { title } = prod;
+        return <div>{title}</div>;
+      })}
     </div>
   );
 };
