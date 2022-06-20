a3d7d058838559689db5e8bd8ff44a1c3e5a6a0e
Ubaid Uz Zaman <wahidshams@gmail.com>
2022-06-20 14:52:10 +0530

    added some features

--- src/features/products/productSlice.js
+++ src/features/products/productSlice.js
@@ -5,14 +5,14 @@ const initialState = {
   productList: [],
 };
 export const fetchProducts = createAsyncThunk("product/fetchProducts", () => {
-  return axios.get("https://dummyjson.com/products");
+  return axios.get("http://localhost:5000/products");
 });
 const productSlice = createSlice({
   name: "product",
   initialState,
   extraReducers: builder => {
     builder.addCase(fetchProducts.fulfilled, (state, action) => {
-      state.productList = action.payload.data.products;
+      state.productList = action.payload.data;
     });
   },
 });
