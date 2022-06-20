d7e55ceaa51a391602a2e6b8e349de2aa83a92a8
amruta <amrutasadalgi@gmail.com>
2022-06-20 15:07:47 +0530

    kids comit

--- src/components/categories/Kids.jsx
+++ src/components/categories/Kids.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import Cataxios from "../../apis/Cataxios";
-
+import style from "./cat.module.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BiSort } from "react-icons/bi";
+import { FaHeart, FaFilter } from "react-icons/fa";
 const Kids = () => {
-  let [data, setdata] = useState();
+  let [data, setdata] = useState([]);
   const fetchdata = async () => {
     let { data } = await Cataxios.get("/products");
     setdata(data);
@@ -17,14 +20,31 @@ const Kids = () => {
   return (
     <div>
       <div>
-        <h1>Kids Category</h1>
         <div>
-          <img src="" alt="" />
-          <h1></h1>
-          <p></p>
-          <p></p>
-          <button></button>
-          <button></button>
+          <h1>Kids Category</h1>
+          <div className={style.block}>
+            <input type="text" name="" id="" />
+            <p>
+              <AiOutlineSearch />
+            </p>
+            <p><BiSort/></p>
+            <p><FaFilter/></p>
+            <p><FaHeart/></p>
+          </div>
+        </div>
+        <div className={style.box}>
+          {data.map((data) => {
+            let { productsid, brand, rating, thumbnail_URL, price } = data;
+            return (
+              <div>
+                <img src={thumbnail_URL} alt="" />
+                <p>ID:{productsid}</p>
+                <p>BRAND:{brand}</p>
+                <p>RATING:{rating}</p>
+                <p>PRICE:{price}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
