bf76ba11e88d57639aa8e8e4b72563f0c75896c5
Richter-Anto <richtermullassery@gmail.com>
2022-06-22 14:50:01 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,13 @@ import axios from "axios";
 import CarouselComp from "../../components/Carousel/CarouselComp";
 import FeaturedProducts from "../../components/featured products/FeaturedProducts";
 import Maincategory from "../../components/categories/Maincategory";
+import ChatBot from "../../components/chatBot/ChatBot";
 
 const Home = () => {
   return (
     <section className="homeBlock">
       <article>
+        <ChatBot />
         <CarouselComp />
         <Maincategory />
         <FeaturedProducts />
