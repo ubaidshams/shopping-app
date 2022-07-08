69b6d80df9e6465e2d9df1e86e12d283562a6a77
Manjunath G <manjunathgrandhe15@gmail.com>
2022-07-08 10:33:40 +0530

    address change

--- src/pages/cart/ProductCheckout/SelectAddress.jsx
+++ src/pages/cart/ProductCheckout/SelectAddress.jsx
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import style from "./selectaddress.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import {AiFillDelete}  from "react-icons/ai"
 
 
 
 const SelectAddress = () => {
-  let [proceed, setproceed]=useState(false)
+  let [proceed, setproceed] = useState(false);
+  let [use, setuse] = useState(false);
+  let navigate = useNavigate();
+
   let currUser = useSelector(state => state.user.currentUser);
   console.log(currUser);
   let { firstName, lastName, gender, email, phone, addressList, id } = currUser;
 
+  useEffect(() => {
+
+    let address = currUser;
+    console.log(address);
+    
+  },[])
+
+  let remove = () => {
+    
+  }
+
+  
+
   let handlesubmit = () => {
-    setproceed(!proceed);
-    toast.success("Order Placed Successfully to this Address")
+    if (use === true) {
+      setproceed(!proceed);
+      toast.success("Order Placed Successfully to this Address");
+      navigate("/payment");
+    } else {
+     toast.error("Please select address to be delivered")
+   }
     
 
   }
@@ -29,7 +51,7 @@ const SelectAddress = () => {
         <p style={{ color: "black", fontWeight: "bold" }}>Deliver to:</p>
         <Link to="/addressform">
           {" "}
-          <button>Add New Address</button>
+          <button className={style.adneadd}>Add New Address</button>
         </Link>
       </div>
       <div className="adcon">
@@ -37,26 +59,29 @@ const SelectAddress = () => {
           {firstName} <span>{lastName}</span>
         </h3>
         <p style={{ fontWeight: "lighter" }}>{phone}</p>
-
-        {addressList.map((item, index) => {
+       
+  {addressList.map((item, index) => {
           return (
             <div style={{ display: "flex" }}>
-              <input type="radio" name="address" />
+              <input type="radio" name="address" onClick={() => setuse(!use)} />
               <div className={style.addname}>
-                <h4>{`Address ${index + 1}`}</h4> :
+                <h4>{`Address ${index + 1}`} : &nbsp;  </h4>
                 <p>
                   {item.houseNo} , {item.landMark},{item.street}, {item.city} -
                   {item.pincode}{" "}
                 </p>
               </div>
+              {/* <div className={style.remove} onClick={remove}>
+                <AiFillDelete />
+              </div> */}
             </div>
           );
         })}
       </div>
-      <Link to="/payment">
+     
         {" "}
-        <button onClick={handlesubmit}>Proceed</button>
-      </Link>
+        <button onClick={handlesubmit} className={style.proceed}>Proceed</button>
+     
     </div>
   );
 };
