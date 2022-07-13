5267c8734965a4c39c9d0e05583d8ce8c837dfbf
Manjunath G <manjunathgrandhe15@gmail.com>
2022-07-13 11:04:08 +0530

    again

--- src/pages/cart/ProductCheckout/Payment.jsx
+++ src/pages/cart/ProductCheckout/Payment.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import style from "./payment.module.css";
+import {AiFillRightCircle}  from "react-icons/ai"
 
 const Payment = () => {
+
+    let orderId = Math.floor((Math.random() * 2664645) );
+    console.log(orderId);
+
+
   return (
-    <div>Payment</div>
-  )
+    
+  
+    <div className={style.checkoutcard}>
+      <h1 style={{ textAlign: "center" }}>CheckOut</h1>
+      <div className={style.radiodiv}>
+        <div className={style.arc}>
+          <AiFillRightCircle />
+          Shipping address
+        </div>
+        <div className={style.arc}>
+          {" "}
+          <AiFillRightCircle />
+          Order Details{" "}
+        </div>
+        <div className={style.arc}>
+          {" "}
+          <AiFillRightCircle />``
+          Review Your Order
+        </div>
+      </div>
+
+      <div>
+        <h2 style={{ textAlign: "center" }}>Thank you for your order </h2>
+        <p style={{ textAlign: "center", fontSize: "large" }}>
+          <br />
+          Your order number is <h4> #{`${orderId}`} </h4>  we update you when
+          your order has shipped.{" "}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Payment
